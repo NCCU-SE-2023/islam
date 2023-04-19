48cf185e0c2141da2c53835097b2c8e02cccfa55
Shawn <59354704+spencer900824@users.noreply.github.com>
2023-04-19 16:31:29 +0800

    [New] Use mongo-engine. (#1)
    * [New] Use mongo-engine.
    
    * [Fix] Update requirements.txt
    
    * [Update] Add README.md and config

--- islamapp/app/__init__.py
+++ islamapp/app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from api.test import test_route
 from api.user import user_route
-from database import db
-
+from api.task import task_route
+from database import db, mongo
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
     app.register_blueprint(test_route)
     app.register_blueprint(user_route)
+    app.register_blueprint(task_route)
     db.init_app(app)
+    mongo.init_app(app)
     return app
--- islamapp/config.py
+++ islamapp/config.py
@@ -2,4 +2,13 @@ import os
 
 
 class Config:
+    # MySQL
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    # MongoDB
+    MONGODB_SETTINGS: dict = {
+        'db': 'islam',
+        'host': os.getenv("MONGO_HOST") or "127.0.0.1",
+        'port': int(os.getenv("MONGO_PORT")) or 27017,
+    }
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
