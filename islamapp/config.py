0977471b594127042ce4028e6d25d585e3a4961a
spencer900824 <spencer900824@gmail.com>
2023-04-19 10:38:18 +0800

    [New] Use mongo-engine.

--- islamapp/config.py
+++ islamapp/config.py
@@ -2,4 +2,13 @@ import os
 
 
 class Config:
+    # MySQL
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    # MongoDB
+    MONGODB_SETTINGS: dict = {
+        'db': 'islam',
+        'host': 'localhost',
+        'port': 27017
+    }
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
--- islamapp/database.py
+++ islamapp/database.py
@@ -1,3 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_mongoengine import MongoEngine
 
 db = SQLAlchemy()
+mongo = MongoEngine()
