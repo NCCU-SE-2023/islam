9c0c74d211d4c44391bf873ccf1b51ef7ec1048b
spencer900824 <spencer900824@gmail.com>
2023-04-22 17:47:00 +0800

    [New] Columns: priority, retry_times.

--- islamapp/model/task.py
+++ islamapp/model/task.py
@@ -1,14 +1,18 @@
 from mongoengine import fields, Document
 
 class Task(Document):
+    # constant field
     task_id = fields.StringField(primary_key=True, required=True)
-    status = fields.StringField(required=True)
     type = fields.StringField(required=True)
-    task_detail = fields.DictField(required=False)
     create_user = fields.StringField(required=True)
+    task_detail = fields.DictField(required=False)
     create_at = fields.IntField(required=True)
+    # variable field
+    status = fields.StringField(required=True)
     update_at = fields.IntField(required=True)
     error_msg = fields.DictField(required=False)
+    priority = fields.IntField(required=False, default=0, min_value=-1, max_value=5)
+    retry_times = fields.IntField(required=True, default=0)
 
     def to_json(self):
         returnDict = {}
