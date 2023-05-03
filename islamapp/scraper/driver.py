ed01bbab3f06047a284fce3b0b8391e54ce6e923
spencer900824 <spencer900824@gmail.com>
2023-05-03 22:34:22 +0800

    [Update] Driver now able to run on remote node.

--- islamapp/scraper/driver.py
+++ islamapp/scraper/driver.py
@@ -4,11 +4,17 @@ from webdriver_manager.chrome import ChromeDriverManager as CDM
 class Driver(object):
     """Congigure and return a selenium webdriver instance.
     """
-    def __init__(self):
+    def __init__(self, hub_endpoint=None):
         self.options = webdriver.ChromeOptions()
         self.options.add_argument("--headless")
         self.options.add_argument('--no-sandbox')
-        self.driver = webdriver.Chrome(CDM.install(), options = self.options)
-    def __delete__(self):
+        if hub_endpoint:
+            self.is_remote = True
+            self.driver = webdriver.Remote(hub_endpoint, options=self.options)
+        else:
+            self.is_remote = False
+            self.driver = webdriver.Chrome(CDM().install(), options=self.options)
+
+    def __del__(self):
         self.driver.quit()
 
