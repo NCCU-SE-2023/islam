5a2a220e4d2c6f1a330df41013bb8a22aac13bb8
ShawnWu-TBCASoft <shawn.wu@tbcasoft.com>
2023-06-12 09:32:39 +0800

    intergrate controller into main

--- islamapp/main.py
+++ islamapp/main.py
@@ -1,6 +1,24 @@
 from app import create_app
 from config import Config
 
+from scraper.scraper_controller import ScraperCotroller
+import logging
+
+status_logger = logging.getLogger("STATUS_LOGGER")
+status_handler = logging.FileHandler('logs/status.log')
+status_handler.setLevel(logging.INFO)
+status_logger.addHandler(status_handler)
+
+mission_logger = logging.getLogger("MISSION_LOGGER")
+mission_handler = logging.FileHandler('logs/mission.log')
+mission_handler.setLevel(logging.INFO)
+mission_logger.addHandler(mission_handler)
+
+logging.basicConfig(filename='logs/log.log',level=logging.DEBUG,format='%(asctime)s %(levelname)s: %(message)s')
+
+scraper_controller = ScraperCotroller(status_logger, mission_logger, run_period=1, run_local=True, local_driver_limit=4)
+scraper_controller.run()
+
 app = create_app(config=Config)
 
 if __name__ == "__main__":
