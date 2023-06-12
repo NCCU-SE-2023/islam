6b19c92250ceada65d420b80ec0d64f54a005d9f
PIGAsHIT <93385350+PIGAsHIT@users.noreply.github.com>
2023-06-13 03:56:20 +0800

    Update query.py
    add api get_account()

--- islamapp/api/query.py
+++ islamapp/api/query.py
@@ -6,3 +6,7 @@ query_route = Blueprint("query_route", __name__)
 @query_route.route("/api/v1/islam/query", methods=["POST"])
 def query():
     return query_service.query(request)
+
+@query_route.route("/api/v1/islam/query", methods=["GET"])
+def get_account():
+    return query_service.get_account(request)
