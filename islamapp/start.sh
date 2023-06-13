7edbac26091898ae0244a9b0a6ed679a67241234
William <william1006wei@Gmail.com>
2023-06-13 13:38:03 +0800

    [FIX] mongo init fail (still cant) QQ

--- islamapp/start.sh
+++ islamapp/start.sh
@@ -2,11 +2,8 @@
 
 echo "APP_NAME: $APP_NAME, APP_ENV: $APP_ENV"
 
-# Wait for 5 seconds
-sleep 5
+# Call init_db.sh script
+./init_db.sh
 
-# Run alembic
-alembic upgrade head
-
+# Start gunicorn
 gunicorn -w 2 -b 0.0.0.0:5555 main:app --reload
-
--- islamapp/start_debug.sh
+++ islamapp/start_debug.sh
@@ -2,10 +2,6 @@
 
 echo "APP_NAME: $APP_NAME, APP_ENV: $APP_ENV"
 
-# Wait for 5 seconds
-sleep 5
-
-# Run alembic
-alembic upgrade head
+./init_db.sh
 
 python3 -m flask run
