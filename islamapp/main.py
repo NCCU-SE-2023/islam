687a7a502c0ce6ed33afd37ee226603f251297c1
William <william1006wei@Gmail.com>
2023-06-13 13:02:51 +0800

    [ADD] islamapp docker compose

--- islamapp/main.py
+++ islamapp/main.py
@@ -4,4 +4,4 @@ from config import Config
 app = create_app(config=Config)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5555)
--- islamapp/start_debug.sh
+++ islamapp/start_debug.sh
@@ -8,5 +8,4 @@ sleep 5
 # Run alembic
 alembic upgrade head
 
-gunicorn -w 2 -b 0.0.0.0:5555 main:app --reload
-
+python3 -m flask run
