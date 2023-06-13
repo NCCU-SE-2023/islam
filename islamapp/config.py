bc06201879d2cd556d89b97a7c2ba312f483a840
William <william1006wei@Gmail.com>
2023-06-13 21:49:39 +0800

    [FIX] make local can connect to mongo and mysql

--- islamapp/config.py
+++ islamapp/config.py
@@ -7,13 +7,13 @@ dotenv.load_dotenv()
 class Config:
     # MySQL
     SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "mysql+pymysql://root:islam@mysql-databse:3306/islam"
+        "DATABASE_URL", "mysql+pymysql://root:islam@127.0.0.1:3306/islam"
     )
 
     # MongoDB
     MONGODB_SETTINGS: dict = {
         "db": os.getenv("MONGO_DB", "islam"),
-        "host": os.getenv("MONGO_HOST", "mongo-databse"),
+        "host": os.getenv("MONGO_HOST", "127.0.0.1"),
         "port": int(os.getenv("MONGO_PORT", "27017")),
         "username": "islam",
         "password": "islam",
