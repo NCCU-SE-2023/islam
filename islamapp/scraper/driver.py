b46029454c7efd88a9f8c3a1fc8a7615520e4f1b
ShawnWu-TBCASoft <shawn.wu@tbcasoft.com>
2023-06-13 16:28:02 +0800

    [Test] pre commit 2

--- islamapp/scraper/driver.py
+++ islamapp/scraper/driver.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager as CDM
 
+
 class Driver(object):
-    """Congigure and return a selenium webdriver instance.
-    """
+    """Congigure and return a selenium webdriver instance."""
+
     def __init__(self, hub_endpoint=None):
         self.options = webdriver.ChromeOptions()
         self.options.add_argument("--headless")
-        self.options.add_argument('--no-sandbox')
-        self.options.add_argument('window-size=1920x1080')
-        self.options.add_argument('--start-maximized')
+        self.options.add_argument("--no-sandbox")
+        self.options.add_argument("window-size=1920x1080")
+        self.options.add_argument("--start-maximized")
         if hub_endpoint:
             self.is_remote = True
             self.driver = webdriver.Remote(hub_endpoint, options=self.options)
@@ -19,4 +20,3 @@ class Driver(object):
 
     def __del__(self):
         self.driver.quit()
-
--- islamapp/tests/data_model_tests/test_task.py
+++ islamapp/tests/data_model_tests/test_task.py
@@ -3,43 +3,44 @@ import unittest
 from mongoengine import connect, QuerySet
 from model.task import Task
 
-#generate the unittest for the user_followers method
+# generate the unittest for the user_followers method
 class TestTask(unittest.TestCase):
-    
     def setUp(self):
-        connect('islam')
-        self.test_task=Task.create_task("test_user", "FOLLOWERS", {"test_detail": "test_detail"})        
-    
+        connect("islam")
+        self.test_task = Task.create_task(
+            "test_user", "FOLLOWERS", {"test_detail": "test_detail"}
+        )
+
     def test_create_task(self):
-        create_task=Task.create_task("test_user", "FOLLOWERS", {"test_detail": "test_detail"})
+        create_task = Task.create_task(
+            "test_user", "FOLLOWERS", {"test_detail": "test_detail"}
+        )
         self.assertIsInstance(create_task, Task)
-        
+
     def test_set_status(self):
         self.test_task.set_status("ERROR")
-        self.assertEqual(self.test_task.status,"ERROR")
-    
+        self.assertEqual(self.test_task.status, "ERROR")
+
     def test_set_error_msg(self):
-        self.test_task.set_error_msg({"error msg" : "test error msg"})
-        self.assertEqual(self.test_task.error_msg,{"error msg" : "test error msg"})
-    
+        self.test_task.set_error_msg({"error msg": "test error msg"})
+        self.assertEqual(self.test_task.error_msg, {"error msg": "test error msg"})
+
     def test_set_start_time(self):
         self.test_task.retry_task()
         self.assertEqual(self.test_task.retry_times, 1)
-    
+
     def test_get_by_status(self):
         get_by_status = Task.get_by_status("ERROR")
         self.assertIsInstance(get_by_status, QuerySet)
-    
+
     def test_get_by_create_user(self):
         get_by_create_user = Task.get_by_create_user("test_user")
         self.assertIsInstance(get_by_create_user, QuerySet)
-    
+
     def test_get_by_id(self):
         get_by_id = Task.get_by_id(self.test_task.task_id)
         self.assertIsInstance(get_by_id, Task)
-         
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
-
-
-
